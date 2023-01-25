dc83751814749641e004dc55a79a1b5cf4101abc
Naresh7167 <Naresh.Gangireddy@data-axle.com>
2023-01-25 17:51:11 +0530

    bug fix for the dashboard incident api

--- common/views.py
+++ common/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from account.permissions import IsBusinessUnitUser
 
-from common.models import Businessunits, Components, ComponentsStatus, IncidentsActivity, Sidebar, UserBusinessunits
+from common.models import Businessunits, Components, ComponentsStatus, IncidentComponent, Incidents, IncidentsActivity, Sidebar, UserBusinessunits
 from account.permissions import BaseStAppPermission
 from common.utils import component_group_order, get_components_all_list
 
@@ -66,20 +66,35 @@ def get_dashboard_incident_component_status(request):
     l_business_unit = Businessunits.objects.filter(
         businessunit_name=request.headers.get('businessunit')).first().businessunit_id
     component_status_incident = []
-    l_status_list = ComponentsStatus.objects.values_list(
-        'component_status_name', flat=True)
-    if l_status_list:
-        for l_status in l_status_list:
+    component_qs = Components.objects.filter(
+        is_active=True, businessunit_id=l_business_unit, has_subgroup=False)
+    if component_qs:
+        for component_data in component_qs:
             status_dict = {}
-            l_IncidentsActivity = IncidentsActivity.objects.filter(
-                component_status=l_status, businessunit_id=l_business_unit).order_by('-modified_datetime').first()
-            if l_IncidentsActivity:
-                status_dict['incident_id'] = l_IncidentsActivity.incident_id
-                status_dict['component_status'] = l_IncidentsActivity.component_status
-                status_dict['component_id'] = l_IncidentsActivity.component_id
+            incident_component_obj = IncidentComponent.objects.filter(
+                component_id=component_data.component_id, businessunit_id=l_business_unit, is_active=True, incident__isdeleted=False, incident__is_active=True,).order_by('-modify_datetime').first()
+            if incident_component_obj:
+                status_dict['component_id'] = incident_component_obj.component_id
+                status_dict['component_name'] = component_data.component_name
+                status_dict['component_status'] = component_data.component_status.component_status_name
+                status_dict['incident_id'] = incident_component_obj.incident_id
                 component_status_incident.append(status_dict)
     return Response(component_status_incident, status=status.HTTP_200_OK)
 
 
 def Mytemplates(request):
-    return render(request, 'index.html')
+    l_incident = Incidents.objects.first()
+    components_effected = ['adobe', 'import', 'export', 'yesmail marketing']
+    l_status = str(l_incident.status).capitalize()
+    context = {
+        "incident_data": l_incident,
+        "component_data": components_effected,
+        "user": 'naresh.gangireddy@data-axle.com',
+        "status": l_status,
+        "name": l_incident.name,
+        "message": l_incident.message
+    }
+
+    subject = f"[Data Axle platform status updates] Incident {l_status} - Admin"
+    # return render(request, template_name='incident_email_notification.html', context=context,)
+    return render(request, template_name='start.html', context=context)
